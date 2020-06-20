0fc22f7099e4c29c3da9c7b25f29ccab8596293b
motyliak <67186212+motyliak@users.noreply.github.com>
2020-06-20 18:34:02 +0200

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+def on_button_pressed_a():
+    global auto
+    auto = False
+    strip.show()
+    strip.rotate(1)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global auto
+    auto = True   
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+
+auto = True
+strip: neopixel.Strip = None
 strip = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
 strip.show_rainbow(1, 360)
 
-
 def on_forever():
-    strip.show()
-    strip.rotate(1)
-    basic.pause(100)
-    
+    if auto:
+        strip.show()
+        strip.rotate(1)
+        basic.pause(300)
 basic.forever(on_forever)
-
-
--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
-let strip = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
-strip.showRainbow(1, 360)
-basic.forever(function on_forever() {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    auto = false
     strip.show()
     strip.rotate(1)
-    basic.pause(100)
-    strip.setBrightness(10)
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    auto = true
+})
+let auto = true
+let strip : neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
+strip.showRainbow(1, 360)
+basic.forever(function on_forever() {
+    if (auto) {
+        strip.show()
+        strip.rotate(1)
+        basic.pause(300)
+    }
+    
 })
